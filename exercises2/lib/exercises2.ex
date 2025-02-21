5ab00a9597c1a83b181bbc54c68b1b4aa3588d43
sergiohlazaro <sergio.hlazaro@alumnos.upm.es>
2025-02-21 16:42:53 +0100

    Ejercicios sheet2 completados, todas las pruebas del test completadas con éxito.

--- exercises2/lib/exercises2.ex
+++ exercises2/lib/exercises2.ex
@@ -1,18 +1,105 @@
-defmodule Exercises2 do
-  @moduledoc """
-  Documentation for `Exercises2`.
-  """
+defmodule Sheet2 do
+  # Ejercicio 1: Longitud de una lista
+  def len([]), do: 0
+  def len([_ | tail]), do: 1 + len(tail)
 
-  @doc """
-  Hello world.
+  # Ejercicio 2: Concatenar listas
+  def app([], list2), do: list2
+  def app([head | tail], list2), do: [head | app(tail, list2)]
 
-  ## Examples
+  # Ejercicio 3: Fibonacci (recursivo)
+  def fib(1), do: 1
+  def fib(2), do: 1
+  def fib(n), do: fib(n - 1) + fib(n - 2)
 
-      iex> Exercises2.hello()
-      :world
+  # Ejercicio 4: Máximo Común Divisor (Euclides)
+  def gcd(n, 0), do: n
+  def gcd(n, m), do: gcd(m, rem(n, m))
 
-  """
-  def hello do
-    :world
+  # Ejercicio 5: Fibonacci como lista
+  def fibs(0), do: []
+  def fibs(1), do: [1]
+  def fibs(2), do: [1, 1]
+  def fibs(n) do
+    fibs(n - 1) ++ [Enum.at(fibs(n - 1), -1) + Enum.at(fibs(n - 2), -1)]
   end
+
+  # Ejercicio 6: Reverso de una lista
+  def reverse([]), do: []
+  def reverse([head | tail]), do: reverse(tail) ++ [head]
+
+  # Ejercicio 7: Reverso concatenado con otra lista
+  def revonto([], ys), do: ys
+  def revonto([head | tail], ys), do: revonto(tail, [head | ys])
+
+  # Ejercicio 8: Triángulo de Pascal
+  def pascal(0), do: [1]
+  
+  def pascal(n) do
+    prev_row = pascal(n - 1)
+    middle_values = Enum.chunk_every(prev_row, 2, 1, :discard) |> Enum.map(&Enum.sum/1)
+    [1] ++ middle_values ++ [1]
+  end
+
+  # Ejercicio 9: MergeSort
+  def mergesort([]), do: []
+  def mergesort([x]), do: [x]
+  def mergesort(list) do
+    {left, right} = Enum.split(list, div(length(list), 2))
+    merge(mergesort(left), mergesort(right))
+  end
+
+  defp merge([], right), do: right
+  defp merge(left, []), do: left
+  defp merge([h1 | t1] = left, [h2 | t2] = right) do
+    if h1 <= h2 do
+      [h1 | merge(t1, right)]
+    else
+      [h2 | merge(left, t2)]
+    end
+  end
+
+  # Ejercicio 10: Permutaciones de una lista
+  def permuts([]), do: []
+  def permuts([x]), do: [[x]]
+  def permuts(list), do: for x <- list, rest <- permuts(List.delete(list, x)), do: [x | rest]
+
+  # Ejercicio 11: Operaciones con vectores
+  def vadd([], []), do: []
+  def vadd([h1 | t1], [h2 | t2]), do: [h1 + h2 | vadd(t1, t2)]
+
+  def scale([], _), do: []
+  def scale([h | t], s), do: [h * s | scale(t, s)]
+
+  def dotprod([], []), do: 0
+  def dotprod([h1 | t1], [h2 | t2]), do: h1 * h2 + dotprod(t1, t2)
+
+  # Ejercicio 16: Criba de Eratóstenes (Números primos hasta n)
+  def primes_upto(n) when n < 2, do: []
+  def primes_upto(n) do
+    2..n
+    |> Enum.filter(&is_prime/1)
+  end
+
+  defp is_prime(2), do: true
+  defp is_prime(n) when n > 2 do
+    max_divisor = max(2, :math.sqrt(n) |> floor())  # Evita rangos inválidos
+    Enum.all?(2..max_divisor, fn x -> rem(n, x) != 0 end)
+  end
+
+  # Ejercicio 17: Factorización en números primos
+  def factorize(n), do: factorize(n, 2, [])
+
+  defp factorize(1, _, factors), do: factors
+  defp factorize(n, d, factors) when rem(n, d) == 0, do: factorize(div(n, d), d, [d | factors])
+  defp factorize(n, d, factors), do: factorize(n, d + 1, factors)
+
+  # Ejercicio 18: Árboles binarios de búsqueda - Inserción
+  def tree_insert(:tip, value), do: {:node, :tip, value, :tip}
+  def tree_insert({:node, left, v, right}, value) when value < v, do: {:node, tree_insert(left, value), v, right}
+  def tree_insert({:node, left, v, right}, value), do: {:node, left, v, tree_insert(right, value)}
+
+  # Ejercicio 19: Ordenación de árbol binario (inorder)
+  def inorder(:tip), do: []
+  def inorder({:node, left, v, right}), do: inorder(left) ++ [v] ++ inorder(right)
 end
--- exercises2/test/exercises2_test.exs
+++ exercises2/test/exercises2_test.exs
@@ -1,8 +1,84 @@
-defmodule Exercises2Test do
+defmodule Sheet2Test do
   use ExUnit.Case
-  doctest Exercises2
 
-  test "greets the world" do
-    assert Exercises2.hello() == :world
+  test "fib_1", do: assert Sheet2.fib(1) == 1
+  test "fib_2", do: assert Sheet2.fib(2) == 1
+  test "fib_6", do: assert Sheet2.fib(6) == 8
+
+  test "gcd_28_0", do: assert Sheet2.gcd(28,0) == 28
+  test "gcd_1071_462", do: assert Sheet2.gcd(1071,462) == 21
+
+  test "reverse_1", do: assert Sheet2.reverse([1,2,3]) == [3,2,1]
+  test "reverse_2", do: assert Sheet2.reverse([1,2,3,2,2]) == [2,2,3,2,1]
+
+  test "revonto_1", do: assert Sheet2.revonto([1,2,3],[4,5]) == Sheet2.reverse([1,2,3])++[4,5]
+
+  test "pascal_0", do: assert Sheet2.pascal(0) == [1]
+  test "pascal_1", do: assert Sheet2.pascal(1) == [1,1]
+  test "pascal_2", do: assert Sheet2.pascal(2) == [1,2,1]
+  test "pascal_3", do: assert Sheet2.pascal(3) == [1,3,3,1]
+  test "pascal_4", do: assert Sheet2.pascal(4) == [1,4,6,4,1]
+
+  test "mergesort_1", do: assert Sheet2.mergesort([1,3,2]) == [1,2,3]
+  test "mergesort_2", do: assert Sheet2.mergesort([]) == []
+  test "mergesort_3", do: assert Sheet2.mergesort([3,2,1]) == [1,2,3]
+  test "mergesort_4", do: assert Sheet2.mergesort([1,3,1,2,1,4,4]) == [1,1,1,2,3,4,4]
+
+  test "permuts_1", do: assert Sheet2.permuts([]) == []
+  test "permuts_2", do: assert Sheet2.permuts([1]) == [[1]]
+  test "permuts_3", do: assert Sheet2.permuts([1,2]) == [[1,2],[2,1]]
+  test "permuts_4", do: assert MapSet.equal?(MapSet.new(Sheet2.permuts([1,2,3])),MapSet.new([[1,2,3],[3,2,1],[1,3,2],[3,1,2],[2,1,3],[2,3,1]]))
+
+  # Testing for equality on floating point numbers is dangerous...
+  test "vadd_1", do: assert Sheet2.vadd([1.0,2.0,3.0],[2.0,3.0,4.0]) == [3.0,5.0,7.0]
+  test "scale_1", do: assert Sheet2.scale([1.0,2.0,3.0],2) == [2.0,4.0,6.0]
+  test "dotprod_1", do: assert Sheet2.dotprod([1.0,2.0,3.0],[2.0,3.0,4.0]) == Enum.sum([2.0,6.0,12.0])
+
+  test "primes_1", do: assert Sheet2.primes_upto(2) == [2]
+  test "primes_2", do: assert Sheet2.primes_upto(3) == [2,3]
+  test "primes_3", do: assert Sheet2.primes_upto(20) == [2,3,5,7,11,13,17,19]
+
+  test "factorize_1", do: assert Enum.sort(Sheet2.factorize(20)) == [2,2,5]
+  test "factorize_2", do: assert Enum.sort(Sheet2.factorize(17)) == [17]
+  test "factorize_3", do: assert Enum.sort(Sheet2.factorize(135)) == [3,3,3,5]
+
+  test "tree_insert0" do
+    assert Sheet2.tree_insert(:tip,10) == {:node, :tip, 10, :tip}
   end
+
+  test "tree_insert1" do
+    assert Sheet2.tree_insert(one_tree(),10) ==
+    {:node,
+     {:node, :tip, 1, :tip},
+     5,
+     {:node,
+      {:node, :tip, 7, {:node, :tip, 10, :tip}},
+      15,
+      {:node, :tip, 20, :tip}
+     }
+    }
+  end
+
+  defp one_tree() do
+    {:node,
+     {:node, :tip, 1, :tip},
+     5,
+     {:node,
+      {:node, :tip, 7, :tip},
+      15,
+      {:node, :tip, 20, :tip}
+     }
+    }
+  end
+
+  test "tree_inorder0" do
+    assert Sheet2.inorder(:tip) == []
+  end
+
+  test "tree_inorder1" do
+    tree = one_tree()
+    assert Sheet2.inorder(tree) == [1,5,7,15,20]
+  end
+
+  # Missing tests for matrix sum, transposition, matrix product
 end
